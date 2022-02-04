6ba833c7587178986db966f7f605493db5e4eba6
Jonatas <jonatassantos152409@gmail.com>
2022-02-04 11:13:48 -0300

    Api

---
+++ index.js
@@ -0,0 +1,17 @@
+// 1 Passo - configuração inicial
+
+const express = require('express');
+
+// 2 passo - inicializando o express
+const app = express();
+
+// 3 passo - forma de ler JSON // middlewares -  são recursos executados entre as nossas requisições e respostas
+
+app.use(
+    
+)
+
+// criar uma rota inicial, (endpoint) inicial para testar no postman
+
+
+// entregar uma porta, para ter acesso aquele software
